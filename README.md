c7eebf3674c6416997694b85621383d3de3c632a
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-06-29 11:28:20 +0530

    set up 1

---
+++ README.md
@@ -0,0 +1,6 @@
+# Focus on frontend rather then setup
+just run `npm i` and *restart the IDE* and you'r good to go 
+## technologies used
+- react 
+- js
+- tailwind v3.4.17
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      Hey there, start creating beautiful websites without any setup!
+    </div>
+  );
+};
+
+export default App;
